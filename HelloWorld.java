f2d644154148045839b5d35772825c72d7aac6f8
Shubham <140605298+PatilShuhbs@users.noreply.github.com>
2023-07-27 12:53:48 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld{
 		System.out.println("----****Hello Shubham!****----");
 		System.out.println("----***GitHub WebHook Project***----");
 		System.out.println("----****WebHook Demo Working Fine!****----");
+		int a = 5, b = 6;
+		System.out.println(a+b);
+		System.out.println(a-b);
 	}
 }
